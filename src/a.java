765fbbeea61d58d97b2fbc6756b9652a7252ad69
Ana Carolina da Silva Correia <1196702435@acd.grupotiradentes.com>
2020-02-07 21:13:08 -0300

    Soma 2

--- src/a.java
+++ src/a.java
@@ -2,8 +2,12 @@
 public class a {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		
+		int a = 10;
+		int b = 20;
+		
+		System.out.println(a + b);
 	}
 
 }
